7292b648c30e13a80ca984a3133becbffd8574ad
Sandro Groganz <sandro@campaignchain.com>
2015-12-15 17:56:16 +0100

    CE-471 Implement basic endpoints

--- Controller/REST/ActivityController.php
+++ Controller/REST/ActivityController.php
@@ -10,7 +10,7 @@
 
 namespace CampaignChain\Activity\EZPlatformBundle\Controller\REST;
 
-use CampaignChain\CoreBundle\Controller\REST\BaseController;
+use CampaignChain\CoreBundle\Controller\REST\BaseModuleController;
 use CampaignChain\CoreBundle\Entity\Activity;
 use CampaignChain\CoreBundle\Entity\Module;
 use FOS\RestBundle\Controller\Annotations as REST;
@@ -20,112 +20,153 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Request\ParamFetcher;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
-class ActivityController extends BaseController
+/**
+ * @REST\NamePrefix("campaignchain_activity_ezplatform_rest_")
+ *
+ * Class ActivityController
+ * @package CampaignChain\Activity\EZPlatformBundle\Controller\REST
+ */
+class ActivityController extends BaseModuleController
 {
+    const CONTROLLER_SERVICE = 'campaignchain.activity.controller.campaignchain.ezplatform.schedule';
+
+    /**
+     * Get a specific eZ Platform object.
+     *
+     * Example Request
+     * ===============
+     *
+     *      GET /api/v1/p/campaignchain/activity-ezplatform/objects/42
+     *
+     * Example Response
+     * ================
+     *
+    [
+        {
+            "location": {
+                "id": 142,
+                "name": "My eZ Platform object",
+                "status": "unpublished",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+            },
+            {
+                "activity": {
+                "id": 42,
+                "equalsOperation": true,
+                "name": "My eZ Platform object",
+                "startDate": "2015-12-20T12:00:00+0000",
+                "status": "open",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+            },
+            {
+                "operation": {
+                "id": 127,
+                "name": "My eZ Platform object",
+                "startDate": "2015-12-20T12:00:00+0000",
+                "status": "open",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+        }
+    ]
+     *
+     * @ApiDoc(
+     *  section="Packages: eZ Platform",
+     *  requirements={
+     *      {
+     *          "name"="id",
+     *          "requirement"="\d+"
+     *      }
+     *  }
+     * )
+     *
+     * @param string $id The ID of an Activity, e.g. '42'.
+     *
+     * @return CampaignChain\CoreBundle\Entity\Bundle
+     */
+    public function getObjectsAction($id)
+    {
+        return $this->getActivity(
+            $id
+        );
+    }
+
     /**
      * Schedule an eZ Platform content object.
      *
      * Example Request
      * ===============
      *
-     *      POST /api/v1/p/campaignchain/activity-ezplatform/activities
+     *      POST /api/v1/p/campaignchain/activity-ezplatform/objects
+     *
+     * Example Input
+     * =============
+     *
+    {
+        "activity":{
+            "name":"My eZ Platform object",
+            "location":100,
+            "campaign":1,
+            "campaignchain_hook_campaignchain_due":{
+                "date":"2015-12-20T12:00:00+0000"
+            },
+            "campaignchain_hook_campaignchain_assignee":{
+                "user":1
+            }
+        }
+    }
      *
      * Example Response
      * ================
      *
-    {
-    "response": {
-    "1": "http://wordpress.amariki.com",
-    "2": "http://www.slideshare.net/amariki_test",
-    "3": "https://global.gotowebinar.com/webinars.tmpl",
-    "4": "https://twitter.com/AmarikiTest1",
-    "5": "https://www.facebook.com/pages/Amariki/1384145015223372",
-    "6": "https://www.facebook.com/profile.php?id=100008874400259",
-    "7": "https://www.facebook.com/profile.php?id=100008922632416",
-    "8": "https://www.linkedin.com/pub/amariki-software/a1/455/616"
-    }
-    }
+    [
+        {
+            "location": {
+                "id": 142,
+                "name": "My eZ Platform object",
+                "status": "unpublished",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+        },
+        {
+            "activity": {
+                "id": 42,
+                "equalsOperation": true,
+                "name": "My eZ Platform object",
+                "startDate": "2015-12-20T12:00:00+0000",
+                "status": "open",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+        },
+        {
+            "operation": {
+                "id": 127,
+                "name": "My eZ Platform object",
+                "startDate": "2015-12-20T12:00:00+0000",
+                "status": "open",
+                "createdDate": "2015-12-15T15:01:13+0000"
+            }
+        }
+    ]
      *
      * @ApiDoc(
      *  section="Packages: eZ Platform"
      * )
      *
-     * @REST\Post()
+     * @REST\Post("/objects")
+     * @ParamConverter("activity", converter="fos_rest.request_body")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function scheduleObjectsAction(Request $request)
+    public function postObjectsAction(Request $request, Activity $activity)
     {
-//        $data = array(
-//            'campaignchain_core_channel' => array(
-//                'campaignchain_core_location' => array(
-//                    'id' => 100,
-//                )
-//            ),
-//            'campaignchain_core_campaign' => array(
-//                'id' => 1,
-//            ),
-//            'campaignchain_core_activity' => array(
-//                'name' => 'About eZ',
-//                'campaignchain_hook_campaignchain_due' => array(
-//                    'date' => '2015-12-20T12:00:00+0000', // Throw error if not within campaign duration.
-//                ),
-//                'campaignchain_hook_campaignchain_assignee' => array(
-//                    'user' => 1,
-//                )
-//            )
-//        );
-
-        try {
-            $data = json_decode($request->getContent());
-
-            // Get the Channel object.
-            $locationService = $this->get('campaignchain.core.location');
-            $location = $locationService->getLocation($data->campaignchain_core_channel->campaignchain_core_location->id);
-
-            // Get the Campaign object.
-            $campaignService = $this->get('campaignchain.core.campaign');
-            $campaign = $campaignService->getCampaign($data->campaignchain_core_campaign->id);
-
-            $activityModuleService = $this->get(
-                'campaignchain.activity.controller.campaignchain.ezplatform.schedule'
-            );
-
-            $activityModuleService->setActivityContext($campaign, $location);
-
-            $activity = new Activity();
-            $activity->setName($data->campaignchain_core_activity->name);
-
-            $form = $this->createForm(
-                $activityModuleService->getActivityFormType('rest'), $activity
-            );
-
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $activity = $activityModuleService->createActivity($activity, $form);
-
-                $routeOptions = array(
-                    'id' => $activity->getId(),
-                    'version' => 'v1',
-                    '_format' => $request->get('_format')
-                );
-
-                $view = $this->routeRedirectView(
-                    'campaignchain_core_rest_activity_get_activities_activity',
-                    $routeOptions,
-                    Response::HTTP_CREATED
-                );
-                //print_r($routeOptions);die();
-                return $this->handleView($view);
-            } else {
-                return $this->errorResponse(
-                    $form
-                );
-            }
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), $e->getCode());
-        }
+        return $this->postActivity(
+            'CampaignChainCoreBundle:REST/Activity:getActivities',
+            $request,
+            $activity
+        );
     }
 }
